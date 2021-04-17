56e9be95900a461f95711656052f5a16c78211f4
leida <zhang20084@126.com>
2021-04-17 09:22:37 +0800

    fix:更换为js-cookie

---
+++ static/js/account.js
@@ -0,0 +1,17 @@
+//third-party login
+let url = sessionStorage['url'];
+let auth = sessionStorage['auth'];
+let from = "";
+if (from != null && from !== '') {
+  Cookies.set('from', from, {
+    expires: 1
+  });
+} else {
+  from = Cookies.get("from") ? Cookies.get("from") : "";
+}
+if (typeof (from) === "undefined") {
+  from = '';
+}
+from = from !== "" ? from : document.referrer;
+from = from.indexOf("account.easyapi.com") >= 0 ? "https://www.easyapi.com" : from;
+let domain = document.domain.indexOf("easyapi") >= 0 ? "easyapi.com" : document.domain;
