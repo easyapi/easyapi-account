9abcb1c99d74782ee0a54138e74d12a74f18a7f8
leida <zhang20084@126.com>
2021-04-21 14:15:15 +0800

    fix:三方登录跳转修改

--- static/js/account.js
+++ static/js/account.js
@@ -3,9 +3,11 @@ let url = sessionStorage.getItem('url');
 let auth = sessionStorage.getItem('auth');
 let from = "";
 if (from != null && from !== '') {
-  Cookies.set('from', from, {
-    expires: 1
-  });
+  if (from.indexOf("easyapi.com") > 0) {
+    Cookies.set('from', from, {
+      expires: 1
+    });
+  }
 } else {
   from = Cookies.get("from") ? Cookies.get("from") : "";
 }
@@ -14,4 +16,5 @@ if (typeof (from) === "undefined") {
 }
 from = from !== "" ? from : document.referrer;
 from = from.indexOf("account.easyapi.com") >= 0 ? "https://www.easyapi.com" : from;
+from = from.indexOf("easyapi.com") >= 0 ? from : "https://www.easyapi.com";
 let domain = document.domain.indexOf("easyapi") >= 0 ? "easyapi.com" : document.domain;
