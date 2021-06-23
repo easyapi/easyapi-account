7893fd074af46a4b2ba72562c6069943489cda85
chenyun123 <1537299563@qq.com>
2021-06-23 11:13:15 +0800

    从url路径上获取from

--- static/js/account.js
+++ static/js/account.js
@@ -1,7 +1,8 @@
 //third-party login
 let url = sessionStorage.getItem('url');
 let auth = sessionStorage.getItem('auth');
-let from = "";
+let from = GetQueryString('from') ? GetQueryString('from') : '';
+
 if (from != null && from !== '') {
   if (from.indexOf("easyapi.com") > 0) {
     Cookies.set('from', from, {expires: 1});
@@ -16,3 +17,9 @@ from = from !== "" ? from : document.referrer;
 from = from.indexOf("account.easyapi.com") >= 0 ? "https://www.easyapi.com" : from;
 from = from.indexOf("easyapi.com") >= 0 ? from : "https://www.easyapi.com";
 let domain = document.domain.indexOf("easyapi") >= 0 ? "easyapi.com" : document.domain;
+
+function GetQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]); return null;
+}
