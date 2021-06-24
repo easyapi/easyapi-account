cf78349aea018783eae057fd3288202251e026b2
leida <zhang20084@126.com>
2021-06-24 11:17:17 +0800

    fix:代码优化

--- static/js/account.js
+++ static/js/account.js
@@ -1,25 +1,29 @@
 //third-party login
 let url = sessionStorage.getItem('url');
 let auth = sessionStorage.getItem('auth');
-let from = GetQueryString('from') ? GetQueryString('from') : '';
 
-if (from != null && from !== '') {
+let from = getQueryString('from');
+
+if (from !== '') {
   if (from.indexOf("easyapi.com") > 0) {
     Cookies.set('from', from, {expires: 1});
   }
 } else {
   from = Cookies.get("from") ? Cookies.get("from") : "";
 }
-if (typeof (from) === "undefined") {
-  from = '';
-}
 from = from !== "" ? from : document.referrer;
 from = from.indexOf("account.easyapi.com") >= 0 ? "https://www.easyapi.com" : from;
 from = from.indexOf("easyapi.com") >= 0 ? from : "https://www.easyapi.com";
 let domain = document.domain.indexOf("easyapi") >= 0 ? "easyapi.com" : document.domain;
 
-function GetQueryString(name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-  var r = window.location.search.substr(1).match(reg);
-  if (r != null) return unescape(r[2]); return null;
+/**
+ * 获取URL参数值
+ */
+function getQueryString(name) {
+  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+  let r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return decodeURIComponent(r[2]);
+  }
+  return '';
 }
