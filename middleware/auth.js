bece0a9ee7827dd7a9de71499fa94c896dbf4a33
shdboom <928421021@qq.com>
2021-11-26 09:56:14 +0800

    bug注释

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,15 @@ export default function({ store, redirect, route }) {
   // let url = sessionStorage.getItem('url');
   // let auth = sessionStorage.getItem('auth');
   let from = route.query.from;
-  if (from !== "") {
+  console.log(from);
+  if (from !== undefined) {
     if (from.indexOf("easyapi.com") > 0) {
       Cookies.set("from", from, { expires: 1 });
     }
   } else {
     from = Cookies.get("from") ? Cookies.get("from") : "";
   }
-  from = from !== "" ? from : document.referrer;
+  // from = from !== "" ? from : document.referrer;
   from = from.indexOf("account.easyapi.com") >= 0 ? "https://www.easyapi.com" : from;
   from = from.indexOf("easyapi.com") >= 0 ? from : "https://www.easyapi.com";
   // let domain = document.domain.indexOf("easyapi") >= 0 ? "easyapi.com" : document.domain;
