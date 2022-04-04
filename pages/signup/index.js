d59d8bbbd7805a55cf68367a3fbc7c6d9a78d0c6
leida <zhang20084@126.com>
2022-04-04 17:32:50 +0800

    fix:注册页面优化

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -18,10 +18,18 @@ export default {
     };
   },
   data() {
+    let validPhoneNumber = (rule, value, callback) => {
+      if (isValidPhoneNumber(value, this.ruleForm.country)) {
+        callback()
+      } else {
+        callback(new Error('手机号码格式有误'))
+      }
+    }
     return {
       areaCodes,
       disabled: true,
-      sendCodeBtn: false,
+      sendCodeBtn: true,//默认不可以发送验证码
+      existUsername: false,//默认用户名不存在
       sendCodeCount: "获取验证码",
       ruleForm: {
         areaCode: 86,
@@ -36,11 +44,7 @@ export default {
       rules: {
         username: [
           {required: true, message: "请输入手机号码", trigger: "blur"},
-          {
-            pattern: /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/,
-            message: "手机号码格式有误",
-            trigger: "blur"
-          }
+          {validator: validPhoneNumber, trigger: "blur"}
         ],
         code: [
           {required: true, message: "请输入验证码", trigger: "blur"},
@@ -50,7 +54,7 @@ export default {
           {required: true, message: "请输入姓名", trigger: "blur"}
         ],
         password: [
-          {required: true, message: "密码6~16位之间，建议包含英文和标点符号", trigger: "blur"},
+          {required: true, message: "请输入密码", trigger: "blur"},
           {min: 6, max: 16, message: "密码6~16位之间，建议包含英文和标点符号", trigger: "blur"}
         ],
         confirmPassword: [
@@ -62,6 +66,10 @@ export default {
   methods: {
     signup() {
       let that = this;
+      if (that.existUsername) {
+        that.$message.error("该账号已注册，请直接登录");
+        return;
+      }
       if (that.ruleForm.password !== that.ruleForm.confirmPassword) {
         that.$message.error("确认密码不一致");
         return;
@@ -70,79 +78,69 @@ export default {
         that.$message.error("请勾选同意EasyAPI用户协议");
         return;
       }
-      if (!isValidPhoneNumber(this.ruleForm.username, this.ruleForm.country) || this.ruleForm.username.length < 11) {
-        that.$message.error("手机号码格式有误");
-        return;
-      }
-      findUsername({username: this.ruleForm.username}, this).then(res => {
-        this.$message.error("该账号已注册，请直接登录");
+      let from = Cookies.get("from")
+      signup(that.ruleForm, this).then(res => {
+        if (res.data.code === 1) {
+          Cookies.set("authenticationToken", res.data.content, {
+            expires: that.ruleForm.rememberMe ? 30 : 0.1,
+            path: "/",
+            domain: Cookies.get("domain")
+          });
+          that.$message.success(res.data.message);
+          setTimeout(() => {
+            Cookies.remove("from");
+            window.location.replace(from);
+          }, 1000);
+        } else {
+          that.$message.error(res.data.message);
+        }
       }).catch(error => {
-        let from = Cookies.get("from")
-        signup(that.ruleForm, this).then(res => {
-          if (res.data.code === 1) {
-            Cookies.set("authenticationToken", res.data.content, {
-              expires: that.ruleForm.rememberMe ? 30 : 0.1,
-              path: "/",
-              domain: Cookies.get("domain")
-            });
-            that.$message.success(res.data.message);
-            setTimeout(() => {
-              Cookies.remove("from");
-              window.location.replace(from);
-            }, 1000);
-          } else {
-            that.$message.error(res.data.message);
-          }
-        }).catch(error => {
-          that.$message.error(error.response.data.message);
-        });
+        that.$message.error(error.response.data.message);
       });
     },
 
-    //发送验证码
+    /**
+     * 发送验证码
+     */
     sendCode() {
+      if (this.sendCodeBtn) {
+        return
+      }
       let that = this;
-      if (!this.ruleForm.username) {
-        this.$message.error("请输入手机号码");
-        return;
-      }
-      if (!isValidPhoneNumber(this.ruleForm.username, this.ruleForm.country) || this.ruleForm.username.length < 11) {
-        this.$message.error("手机号码格式有误");
-        return;
-      }
-      findUsername({username: this.ruleForm.username}, this).then(res => {
-        this.$message.error("该账号已注册，请直接登录");
+      let timer;
+      sendCode({mobile: that.ruleForm.username}, this).then(res => {
+        if (res.data.code === 1) {
+          that.$message.success("验证码发送成功");
+          let second = 60;
+          that.sendCodeBtn = true;
+          timer = setInterval(() => {
+            second--;
+            if (second === 0) {
+              that.sendCodeBtn = false;
+              that.sendCodeCount = "获取验证码";
+              clearInterval(timer);
+              return;
+            }
+            that.sendCodeCount = `剩余${second}秒`;
+          }, 1000);
+        } else {
+          that.$message.error(res.data.message);
+        }
       }).catch(error => {
-        let timer;
-        sendCode({mobile: that.ruleForm.username}, this).then(res => {
-          if (res.data.code === 1) {
-            that.$message.success("验证码发送成功");
-            let second = 60;
-            that.sendCodeBtn = true;
-            timer = setInterval(() => {
-              second--;
-              if (second === 0) {
-                that.sendCodeBtn = false;
-                that.sendCodeCount = "获取验证码";
-                clearInterval(timer);
-                return;
-              }
-              that.sendCodeCount = `剩余${second}秒`;
-            }, 1000);
-          } else {
-            that.$message.error(res.data.message);
-          }
-        }).catch(error => {
-          that.$message.error(error.response.data.message);
-        });
+        that.$message.error(error.response.data.message);
       });
     },
 
-    //在手机号输入正确之后查询是否有此用户
+    /**
+     * 在手机号输入正确之后查询是否有此用户
+     */
     findUsername() {
       findUsername({username: this.ruleForm.username}, this).then(res => {
+        this.existUsername = true
         this.$message.error("该账号已注册，请直接登录");
-      })
+      }).catch(error => {
+        this.sendCodeBtn = false
+      });
     },
   },
   mounted() {
