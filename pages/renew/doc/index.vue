2ffbd48a811ff3a96a45dab9ae321cf51c4f4791
李华熠 <2736495579@qq.com>
2023-05-23 16:27:39 +0800

    完善renew/doc页面

--- pages/renew/doc/index.vue
+++ pages/renew/doc/index.vue
@@ -1,18 +1,15 @@
-<script lang="ts">
-import { onMounted, reactive, watch } from 'vue'
-import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
-import { ElMessage } from 'element-plus'
+<script>
+import { ElMessage, ElMessageBox } from 'element-plus'
+import moment from 'moment'
 import Edition from '../components/Edition.vue'
 import SelectPrice from '../components/SelectPrice.vue'
 import Payment from '../components/Payment.vue'
 import WeChatPay from '../components/WeChatPay.vue'
-import renew from '@/api/renew'
-import team from '@/api/team'
-import userStore from '@/store/user'
-import money from '@/api/money'
-import moment from 'moment'
 
+import userStore from '@/store/user'
+
+import renew from '@/api/renew'
+import money from '@/api/money'
 
 export default defineComponent({
   components: {
@@ -24,53 +21,29 @@ export default defineComponent({
   setup() {
     const router = useRouter()
     const store = userStore()
-    // const { proxy: $vm } = getCurrentInstance()
-
     const data = reactive({
       team: {},
       date: '',
       oldDate: '',
-      priceList: [], //价格列表
+      priceList: [], // 价格列表
       selectMonth: 0,
-      balance: '', //账户余额
-      price: 0, //销售价格
-      payment: '余额支付', //支付方式
-      nowMemberCount: 0, //目前团队人数
-      memberCount: 0, //可改变团队人数
+      balance: '', // 账户余额
+      price: 0, // 销售价格
+      payment: '余额支付', // 支付方式
+      nowMemberCount: 0, // 目前团队人数
+      memberCount: 0, // 可改变团队人数
       wechatPayDialog: false,
       weChatPayUrl: null,
-      totalPrice: 0, //应付金额
-      release: '基础版', //版本
-      edition: Boolean
+      totalPrice: 0, // 应付金额
+      release: '基础版', // 版本
+      edition: false,
     })
 
-    watch(() => data.wechatPayDialog, (val) => {
-      if (!val) {
-        getTeamInfo()
-        data.selectMonth = 0
-      }
-    })
-
-    watch(() => data.memberCount, (val) => {
-      getDocumentPrice()
-    })
-
-    function onCreated() {
-      getTeamInfo()
-      getPriceList()
-      getTeamUserList()
-    }
-    onCreated()
-
-    onMounted(() => {
-      document.title = '文档续费 - EasyAPI'
-    })
-
-    function handleClose() {
+    const handleClose = () => {
       data.wechatPayDialog = false
     }
 
-    function getDocumentPrice() {
+    const getDocumentPrice = () => {
       if (data.selectMonth === 0) {
         return
       }
@@ -85,7 +58,7 @@ export default defineComponent({
       })
     }
 
-    function getTeamUserList() {
+    const getTeamUserList = () => {
       getTeamUserList().then(res => {
         if (res.data.code === 1) {
           data.nowMemberCount = res.data.content.length
@@ -94,7 +67,7 @@ export default defineComponent({
       })
     }
 
-    function getPriceList() {
+    const getPriceList = () => {
       getPriceList().then(res => {
         if (res.data.code === 1) {
           data.priceList = res.data.content
@@ -108,18 +81,17 @@ export default defineComponent({
       })
     }
 
-    function getPayment(event) {
+    const getPayment = (event) => {
       data.payment = event
     }
 
-    function stand(pay) {
+    const stand = (pay) => {
       data.payment = pay
     }
 
-    function getTeamInfo() {
+    const getTeamInfo = () => {
       let teamId = store.team ? store.team.id : ''
-      money.getTeamMoney({ teamId: teamId }).then(res => {
-        console.log(res)
+      money.getTeamMoney({teamId: teamId}).then(res => {
         if (res.data.code === 1) {
           data.balance = res.data.content.balance
           data.team = res.data.content.team
@@ -130,27 +102,25 @@ export default defineComponent({
       })
     }
 
-    function selectEdition(item) {
+    const selectEdition = (item) => {
       data.release = item.name
       getDocumentPrice()
       getPriceList()
     }
 
-    function selectPrice(event) {
+    const selectPrice = (event) => {
       data.selectMonth = event.num
       data.price = event.price
       data.date = moment(data.oldDate).add(event.month, 'months').format('YYYY-MM-DD HH:mm:ss')
       getDocumentPrice()
     }
 
-    function sure() {
+    const sure = () => {
       if (data.selectMonth === 0) {
-        /* Warn: Unknown source: $message */
         ElMessage.warning('请选择续费时长')
         return
       }
-      /* Warn: Unknown source: $confirm */
-      ElMessage.$confirm('你确定续费吗？', '确认购买', {
+      ElMessageBox.confirm('你确定续费吗？', '确认购买', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
@@ -159,40 +129,57 @@ export default defineComponent({
       })
     }
 
-    function determineThePurchase() {
-      let data = {
+    const determineThePurchase = () => {
+      const params = {
         price: Number(data.totalPrice),
         payment: data.payment,
         month: data.selectMonth,
         memberCount: data.memberCount,
-        release: data.release
+        release: data.release,
       }
-      renew(data)
-        .then(res => {
-          if (data.payment === '支付宝') {
-            const { href } = router.resolve({
-              path: '/renew/alipay',
-              query: {
-                form: res.data.content.alipay
-              }
-            })
-            window.open(href, '_blank')
-          } else if (data.payment === '微信支付') {
-            data.weChatPayUrl = `https://api.easyapi.com/qr-code?text=${res.data.codeUrl}&bg=ffffff&appKey=f89UK9X5Q3C2YW2y&appSecret=hr2he5ufz6vw0ikz`
-            data.wechatPayDialog = true
-          }
-          getTeamInfo()
-          data.selectMonth = 0
-          /* Warn: Unknown source: $message */
-          ElMessage.success(res.data.message)
-        })
-        .catch(error => {
+      renew.getPriceList(params).then((res) => {
+        if (data.payment === '支付宝') {
+          const href = router.resolve({
+            path: '/renew/alipay',
+            query: {
+              form: res.data.content.alipay,
+            },
+          })
+          window.open(href, '_blank')
+        } else if (data.payment === '微信支付') {
+          data.weChatPayUrl = `https://api.easyapi.com/qr-code?text=${res.data.codeUrl}&bg=ffffff&appKey=f89UK9X5Q3C2YW2y&appSecret=hr2he5ufz6vw0ikz`
+          data.wechatPayDialog = true
+        }
+        getTeamInfo()
+        data.selectMonth = 0
+        ElMessage.success(res.data.message)
+      })
+        .catch(() => {
           if (data.payment === '' || data.payment === null) {
-            /* Warn: Unknown source: $message */
             ElMessage.warning('请选择支付方式')
           }
         })
     }
+
+
+    watch(() => data.wechatPayDialog, (val) => {
+      if (!val) {
+        getTeamInfo()
+        data.selectMonth = 0
+      }
+    })
+
+    watch(() => data.memberCount, (val) => {
+      getDocumentPrice()
+    })
+
+    onMounted(() => {
+      document.title = '文档续费 - EasyAPI'
+      getTeamInfo()
+      getPriceList()
+      getTeamUserList()
+    })
+
     return {
       ...toRefs(data),
       selectPrice,
@@ -217,21 +204,21 @@ export default defineComponent({
       <div class="renew_content max-w-screen-lg">
         <div class="renew_service">
           <strong class="renew_service_title">选择版本：</strong>
-          <Edition :edition="edition" @event="selectEdition" />
+          <Edition :edition="edition" @event="selectEdition"/>
         </div>
         <a class="edition-tips" target="_blank" href="https://www.easyapi.com/info/price">查看不同版本对比</a>
         <div class="renew_service">
           <strong class="renew_service_title">续费价格：</strong>
-          <SelectPrice ref="selectPrice" :pricelist="priceList" @event="selectPrice" />
+          <SelectPrice ref="selectPrice" :pricelist="priceList" @event="selectPrice"/>
         </div>
         <div class="renew_service">
           <strong class="renew_service_title">支付方式：</strong>
-          <Payment :price="price" :balance="balance" @event="getPayment" />
+          <Payment :price="price" :balance="balance" @event="getPayment"/>
         </div>
         <div class="renew_fl">
           <strong class="renew_service_title">团队人数：</strong>
           <div class="">
-            <el-input-number v-model="memberCount" controls-outside :min="nowMemberCount" />
+            <el-input-number v-model="memberCount" controls-outside :min="nowMemberCount"/>
           </div>
         </div>
         <p style="margin-top: -10px; margin-bottom: 10px; margin-left: 80px; color: #888888; font-size: 12px">
@@ -252,12 +239,12 @@ export default defineComponent({
           </div>
         </div>
         <div class="renew_fa">
-          <el-button v-if="data.balance >= data.price || payment !== '余额支付'"
-            style="border-radius: 4px; background-color: #1cc0d6; color: #fff; font-size: 14px" @click="sure">
+          <el-button v-if="balance >= price || payment !== '余额支付'"
+                     style="border-radius: 4px; background-color: #1cc0d6; color: #fff; font-size: 14px" @click="sure">
             确定购买
           </el-button>
-          <el-button v-if="data.balance < data.price && payment === '余额支付'"
-            style="border-radius: 4px; background-color: #1cc0d6; color: #fff; font-size: 14px" disabled>
+          <el-button v-if="balance < price && payment === '余额支付'"
+                     style="border-radius: 4px; background-color: #1cc0d6; color: #fff; font-size: 14px" disabled>
             确定购买
           </el-button>
           <span
@@ -266,6 +253,53 @@ export default defineComponent({
       </div>
     </div>
     <!-- 微信支付弹窗 -->
-    <WeChatPay v-model:visible="wechatPayDialog" :we-chat-pay-url="weChatPayUrl" :price="price" />
+    <WeChatPay v-model:visible="wechatPayDialog" :we-chat-pay-url="weChatPayUrl" :price="price"/>
   </div>
 </template>
+<style lang="scss" scoped>
+.renew_content {
+  height: auto;
+  margin: 0 auto;
+}
+
+.renew_service {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  margin-top: 30px;
+}
+
+.renew_service_title {
+  width: 8%;
+  height: 100%;
+  display: block;
+  color: #323232;
+  font-size: 14px;
+}
+
+.edition-tips {
+  margin-left: 8%;
+  margin-top: 10px;
+  font-size: 12px;
+  color: #1bc0d5;
+}
+
+.renew_fl {
+  width: 100%;
+  height: 55px;
+  display: flex;
+}
+
+.renew_fa {
+  width: 100%;
+  height: 80px;
+  padding-left: 80px;
+}
+
+.renew_fa button {
+  width: 120px;
+  height: 40px;
+  text-align: center;
+}
+
+</style>
