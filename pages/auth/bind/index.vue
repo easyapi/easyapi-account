e35f5e375111cbaa3f9fb37263e3fa70ca310f1c
wjhcjy999 <1040041660@qq.com>
2023-05-25 17:52:11 +0800

    绑定

--- pages/auth/bind/index.vue
+++ pages/auth/bind/index.vue
@@ -1,12 +1,19 @@
 <script>
-import {useCookies} from '@vueuse/integrations/useCookies'
-import {isValidPhoneNumber} from 'libphonenumber-js'
-import {ElMessage} from 'element-plus'
+import { useCookies } from '@vueuse/integrations/useCookies'
+import { isValidPhoneNumber } from 'libphonenumber-js'
+import { ElMessage } from 'element-plus'
 import bind from '@/api/bind'
 import login from '@/api/login'
 
 export default defineComponent({
   setup() {
+    const validPhoneNumber = (rule, value, callback) => {
+      if (isValidPhoneNumber(value, data.ruleForm.country)) {
+        callback()
+      } else {
+        callback(new Error('手机号码格式有误'))
+      }
+    }
     const route = useRoute()
     const data = reactive({
       areaCodes,
@@ -15,28 +22,34 @@ export default defineComponent({
       providerUserId: '',
       providerId: '',
       options: [],
+      ruleRef: null,
       ruleForm: {
         areaCode: 86,
-        country: null,
+        country: 'CN',
         username: '',
         password: '',
-        rememberMe: true,
+        rememberMe: true
       },
       rules: {
         username: [
-          {required: true, message: '请输入手机号码', trigger: 'blur'},
-          {validator: isValidPhoneNumber, trigger: 'blur'},
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: validPhoneNumber, trigger: 'blur' },
         ],
         password: [
-          {required: true, message: '密码6~16位之间,建议包含英文和标点符号', trigger: 'blur'},
-          {min: 6, max: 16, message: '密码6~16位之间,建议包含英文和标点符号', trigger: 'blur'},
+          { required: true, message: '密码6~16位之间,建议包含英文和标点符号', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码6~16位之间,建议包含英文和标点符号', trigger: 'blur' },
         ],
       },
     })
 
-    onUpdated(() => {
-      data.disabled = !(isValidPhoneNumber(data.ruleForm.username, data.ruleForm.country) && data.ruleForm.password.length >= 6)
-    })
+    watch(
+      () => data.ruleForm,
+      () => {
+        data.disabled = !(isValidPhoneNumber(data.ruleForm.username, data.ruleForm.country) && data.ruleForm.password.length >= 6)
+      },
+      { deep: true }
+    )
+
     const bindPost = () => {
       const from = useCookies().get('from')
       const params = {
@@ -44,8 +57,8 @@ export default defineComponent({
         providerId: data.providerId,
       }
       bind.postBind(params).then((res) => {
-        if (res.data.code !== 1) {
-          ElMessage.error(res.data.message)
+        if (res.code !== 1) {
+          ElMessage.error(res.message)
           return
         }
         window.location.replace(from)
@@ -53,22 +66,24 @@ export default defineComponent({
     }
 
     const loginPost = () => {
-      login.postLogin().then((res) => {
-        if (res.data.code !== 1) {
-          ElMessage.error(res.data.message)
+      login.postLogin(data.ruleForm).then((res) => {
+        if (res.code !== 1) {
+          ElMessage.error(res.message)
         }
-        useCookies().set('authenticationToken', res.data.content.idToken, {
-          expires: data.ruleForm.rememberMe ? 30 : 0.1,
+        useCookies().set('authenticationToken', res.content.idToken, {
+          maxAge: data.formData.rememberMe ? 60 * 60 * 24 * 30 : 1,
           path: '/',
           domain: useCookies().get('domain'),
         })
         bindPost()
         ElMessage.success('绑定成功')
       })
-        .catch((error) => {
-          ElMessage.error(error.response.data.message)
-        })
+        // .catch((error) => {
+        //   ElMessage.error(error.response.data.message)
+        // })
     }
+
+
     onMounted(() => {
       const providerUserId = route.query.providerUserId
       const providerId = route.query.providerId
@@ -93,42 +108,68 @@ export default defineComponent({
 <template>
   <div class="main">
     <div class="form">
-      <div class="headline">
-        绑定账号
-      </div>
+      <div class="headline">绑定账号</div>
       <div class="lg-subtitle mb-10">
         第一次使用
         <span>{{ providerId === 'wechat' ? '微信' : 'QQ' }}</span>
         帐号登录，您需要填写账号和密码，以后您也可以使用此账号和密码登录
       </div>
       <div class="ath mb-10">
-        <img class="image-png" :src="imageSrc" alt="">
-        <img class="image-svg" src="@/assets/images/static/svg/bind.svg" alt="">
-        <img class="image-png" src="@/assets/images/static/easyapi.png" alt="">
+        <img class="image-png" :src="imageSrc" alt="" />
+        <img
+          class="image-svg"
+          src="@/assets/images/static/svg/bind.svg"
+          alt=""
+        />
+        <img
+          class="image-png"
+          src="@/assets/images/static/easyapi.png"
+          alt=""
+        />
       </div>
-      <el-form ref="ruleForm" :model="ruleForm" :rules="rules">
+      <el-form ref="ruleRef" :model="ruleForm" :rules="rules">
         <el-form-item label="" prop="username">
-          <el-input v-model="ruleForm.username" placeholder="请输入手机号码" class="input-with-select" maxlength="11">
-            <template slot="prepend">
-              +&nbsp;
-            </template>
-            <el-select slot="prepend" v-model="ruleForm.areaCode" filterable allow-create style="width: 80px">
-              <el-option v-for="item in areaCodes" :key="item.value" :value="item.value">
-                {{ item.label }}(+{{
-                  item.value
-                }})
+          <el-input
+            v-model="ruleForm.username"
+            placeholder="请输入手机号码"
+            class="input-with-select"
+            maxlength="11"
+          >
+            <template> +&nbsp; </template>
+            <el-select
+              v-model="ruleForm.areaCode"
+              filterable
+              allow-create
+              style="width: 80px"
+            >
+              <el-option
+                v-for="item in areaCodes"
+                :key="item.value"
+                :value="item.value"
+              >
+                {{ item.label }}(+{{ item.value }})
               </el-option>
             </el-select>
           </el-input>
         </el-form-item>
         <el-form-item label="" prop="password">
-          <el-input v-model="ruleForm.password" type="password" placeholder="请输入密码"/>
+          <el-input
+            v-model="ruleForm.password"
+            type="password"
+            placeholder="请输入密码"
+          />
         </el-form-item>
         <el-checkbox v-model="ruleForm.rememberMe" class="checkbox" checked>
           记住密码
         </el-checkbox>
-        <el-button id="btn_sub" style="width: 100%" type="primary" :disabled="disabled"
-                   class="btn-block btn btn-lg btn-info" @click="login">
+        <el-button
+          id="btn_sub"
+          style="width: 100%"
+          type="primary"
+          :disabled="disabled"
+          class="btn-block btn btn-lg btn-info"
+          @click="loginPost"
+        >
           绑定账号
         </el-button>
       </el-form>
--- pages/email-upgrade/index.vue
+++ pages/email-upgrade/index.vue
@@ -4,6 +4,8 @@ import { useCookies } from '@vueuse/integrations/useCookies'
 import { ElMessage } from 'element-plus'
 import { isValidPhoneNumber } from 'libphonenumber-js'
 import email from '@/api/email'
+import signup from '@/api/signup'
+
 
 export default defineComponent({
   setup() {
@@ -15,12 +17,11 @@ export default defineComponent({
       ruleForm: null,
       formData: {
         areaCode: 86,
-        country: undefined,
-        username: '',
+        country: 'CN',
         email: '',
         mobile: '',
         code: '',
-        password: '',
+        password: ''
       },
       emailRegex: /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, // 邮箱正则表达式
       rules: {
@@ -51,50 +52,68 @@ export default defineComponent({
       },
     })
 
-    onUpdated(() => {
-      // 校验
-      data.disabled = !(
-        data.emailRegex.test(data.formData.email)
-        && isValidPhoneNumber(data.formData.mobile, data.formData.country)
-        && data.formData.password.length >= 6
-        && data.formData.code.length === 6
-      )
-    })
+    // onUpdated(() => {
+    //   // 校验
+    //   data.disabled = !(
+    //     data.emailRegex.test(data.formData.email)
+    //     && isValidPhoneNumber(data.formData.mobile, data.formData.country)
+    //     && data.formData.password.length >= 6
+    //     && data.formData.code.length === 6
+    //   )
+    // })
+
+    watch(
+      () => data.formData,
+      () => {
+        data.disabled = !(
+          data.emailRegex.test(data.formData.email) &&
+          isValidPhoneNumber(data.formData.mobile, data.formData.country) &&
+          data.formData.password.length >= 6 &&
+          data.formData.code.length === 6
+        )
+      },
+      { deep: true }
+    )
+
+
     const upgradeEmail = () => {
       const from = useCookies().get('from')
       email.upgradeEmail(data.formData)
         .then((res) => {
-          if (res.data.code === 1) {
-            ElMessage.success(res.data.message)
+          if (res.code === 1) {
+            ElMessage.success(res.message)
             setTimeout(() => {
               useCookies().remove('from')
               window.location.replace(from)
             }, 1000)
           } else {
-            ElMessage.error(res.data.message)
+            ElMessage.error(res.message)
           }
         })
-        .catch((error) => {
-          ElMessage.error(error.response.data.message)
-        })
+      // .catch((error) => {
+      //   ElMessage.error(error.response.data.message)
+      // })
     }
 
+    /**
+     * 发送验证码
+     */
     const sendCode = () => {
       const telStr = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
       if (telStr.test(data.formData.mobile)) {
         const params = {
           mobile: data.formData.mobile,
         }
-        email.upgradeEmail(params)
+        signup.sendCodeFn(params)
           .then((res) => {
-            if (res.data.code === 1)
-              ElMessage.success(res.data.message)
+            if (res.code === 1)
+              ElMessage.success(res.message)
             else
-              ElMessage.error(res.data.message)
+              ElMessage.error(res.message)
           })
-          .catch((error) => {
-            ElMessage.error(error.response.data.message)
-          })
+        // .catch((error) => {
+        //   ElMessage.error(error.response.data.message)
+        // })
         let second = 60
         data.sendCodeBtn = true
         const timer = setInterval(() => {
@@ -123,23 +142,36 @@ export default defineComponent({
 <template>
   <div class="main">
     <div class="form">
-      <div class="headline">
-        邮箱升级
-      </div>
+      <div class="headline">邮箱升级</div>
       <el-form ref="ruleForm" :model="formData" :rules="rules">
         <el-form-item label="" prop="email">
           <el-input v-model="formData.email" placeholder="请输入原邮箱账号" />
         </el-form-item>
         <el-form-item label="" prop="password">
-          <el-input v-model="formData.password" placeholder="请输入原邮箱密码" type="password" />
+          <el-input
+            v-model="formData.password"
+            placeholder="请输入原邮箱密码"
+            type="password"
+          />
         </el-form-item>
         <el-form-item label="" prop="mobile">
-          <el-input v-model="formData.mobile" placeholder="请输入新手机号码" maxlength="11">
-            <template slot="prepend">
-              +&nbsp;
-            </template>
-            <el-select slot="prepend" v-model="formData.areaCode" filterable allow-create style="width: 80px">
-              <el-option v-for="item in areaCodes" :key="item.value" :value="item.value">
+          <el-input
+            v-model="formData.mobile"
+            placeholder="请输入新手机号码"
+            maxlength="11"
+          >
+            <template> +&nbsp; </template>
+            <el-select
+              v-model="formData.areaCode"
+              filterable
+              allow-create
+              style="width: 80px"
+            >
+              <el-option
+                v-for="item in areaCodes"
+                :key="item.value"
+                :value="item.value"
+              >
                 {{ item.label }}(+{{ item.value }})
               </el-option>
             </el-select>
@@ -147,22 +179,28 @@ export default defineComponent({
         </el-form-item>
         <el-form-item label="" prop="code">
           <el-input
-            v-model="formData.code" class="code" placeholder="请输入验证码" maxlength="6"
+            v-model="formData.code"
+            class="code"
+            placeholder="请输入验证码"
+            maxlength="6"
             onkeyup="value=value.replace(/[^\d]/g,'')"
           />
           <el-button :disabled="sendCodeBtn" class="getCode" @click="sendCode">
             {{ sendCodeCount }}
           </el-button>
         </el-form-item>
-        <el-button style="width: 100%" type="primary" :disabled="disabled" @click="upgradeEmail">
+        <el-button
+          style="width: 100%"
+          type="primary"
+          :disabled="disabled"
+          @click="upgradeEmail"
+        >
           升级
         </el-button>
       </el-form>
 
       <div class="other-box">
-        <nuxt-link to="/login">
-          我已升级成功，直接登录
-        </nuxt-link>
+        <nuxt-link to="/login"> 我已升级成功，直接登录 </nuxt-link>
       </div>
     </div>
   </div>
