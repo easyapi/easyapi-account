6ae6a45a62452bc5f283bbdee824a2952a082c85
wjhcjy999 <1040041660@qq.com>
2023-05-26 15:15:20 +0800

    doc

--- utils/from.ts
+++ utils/from.ts
@@ -4,19 +4,19 @@ import { userStore } from '~/store/user'
 /**
  * 处理来源链接和域名
  */
-export async function from() {
-  useCookies().set('from', 'http://localhost:3000/renew/service')
-  // let fromData = ''
-  // if (!useRoute().query.from) {
-  //   fromData = await useCookies().get('from') ? useCookies().get('from') : 'https://www.easyapi.com'
-  //   fromData = fromData.includes('account.easyapi.com') ? 'https://www.easyapi.com' : fromData
-  //   useCookies().set('from', fromData)
-  // } else {
-  //   fromData = await useRoute().query.from as string
-  //   if (fromData && fromData.includes('account.easyapi.com'))
-  //     useCookies().set('from', 'https://www.easyapi.com')
-  //   else
-  //     useCookies().set('from', from)
-  // }
+export  function from() {
+  // useCookies().set('from', 'http://localhost:3000/renew/service')
+  let fromData = ''
+  if (!useRoute().query.from) {
+    fromData =  useCookies().get('from') ? useCookies().get('from') : 'https://www.easyapi.com'
+    fromData = fromData.includes('account.easyapi.com') ? 'https://www.easyapi.com' : fromData
+    useCookies().set('from', fromData)
+  } else {
+    fromData =  useRoute().query.from as string
+    if (fromData && fromData.includes('account.easyapi.com'))
+      useCookies().set('from', 'https://www.easyapi.com')
+    else
+      useCookies().set('from', from)
+  }
   useCookies().set('domain', userStore().user.domain)
 }
