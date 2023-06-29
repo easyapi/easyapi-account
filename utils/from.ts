c23ad4b6ea4c7e77e6f92fc0dde25d99047b03a2
leida <zhang20084@126.com>
2023-06-29 16:17:15 +0800

    fix:跳转优化

--- utils/from.ts
+++ utils/from.ts
@@ -6,17 +6,17 @@ import { userStore } from '~/store/user'
  * 处理来源链接和域名
  */
 export function from() {
-  let fromData = ''
+  let url = ''
   if (!useRoute().query.from) {
-    fromData = useCookies().get('from') ? useCookies().get('from') : 'https://www.easyapi.com'
-    fromData = fromData?.includes('account.easyapi.com') ? 'https://www.easyapi.com' : fromData
-    useCookies().set('from', fromData)
+    url = useCookies().get('from') ? useCookies().get('from') : 'https://www.easyapi.com'
+    url = url?.includes('account.easyapi.com') ? 'https://www.easyapi.com' : url
+    useCookies().set('from', url, { path: '/' })
   } else {
-    fromData = useRoute().query.from as string
-    if (fromData?.includes('account.easyapi.com'))
-      useCookies().set('from', 'https://www.easyapi.com')
+    url = useRoute().query.from as string
+    if (url?.includes('account.easyapi.com'))
+      useCookies().set('from', 'https://www.easyapi.com', { path: '/' })
     else
-      useCookies().set('from', fromData)
+      useCookies().set('from', url, { path: '/' })
   }
-  useCookies().set('domain', userStore().domain)
+  useCookies().set('domain', userStore().domain, { path: '/' })
 }
